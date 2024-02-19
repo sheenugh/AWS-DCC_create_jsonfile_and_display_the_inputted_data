d9bff5f1385ca294005f3cd7303a47d1135f5a44
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-02-20 01:01:47 +0800

    pseudo code

---
+++ create_jsonfile_and_display_the_inputted_data.py
@@ -0,0 +1,8 @@
+# ========= IMPORTS/S =========
+
+# ========= PSUEDO CODE =========
+# - Ask user his/her name, age, fav food
+# - Code for storing the user's inputted data
+# - Code for storing the json data
+# - Coed for creating a new json file
+# - Printing the "Data has been saved to "
